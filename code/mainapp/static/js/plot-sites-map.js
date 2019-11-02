22fe19e62fc5491ef5f6066c08107f9ddd4766cc
Vismay <vismayhr@hotmail.com>
2019-11-02 15:43:56 -0300

    Added toggle button for maps. Loads the sitesMap by default when the page is loaded

---
+++ code/mainapp/static/js/plot-sites-map.js
@@ -0,0 +1,44 @@
+function plotSitesMap(data){
+	console.log("In the new plot sites map!")
+	var mymap = L.map('sitesDataMap').setView([62.0, -96.0], 3);
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+		attribution: '<a href="https://www.openstreetmap.org/">OpenStreetMap</a> | <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox.streets',
+		accessToken: 'pk.eyJ1IjoidmlzbWF5aHIiLCJhIjoiY2syZXZ3ZjhrMGUxMjNib2Z1dnpoem5sZCJ9.vhBE1EO3Lxa6TJBy5XyaqQ'
+	}).addTo(mymap);
+	var jsonData = data;
+	var sites_data = jsonData['sites_data']
+	for(row=0; row < sites_data.length; row++){
+		site = sites_data[row]['name'];
+		value = parseFloat(sites_data[row]['value']);
+		latitude = parseFloat(sites_data[row]['lat']);
+		longitude = parseFloat(sites_data[row]['lon']);
+		console.log(site, value, latitude, longitude);
+
+		var circle = L.circle([latitude, longitude], {
+			color: circleOutlineColour(site),
+			fillColor: circleFillColour(site),
+			fillOpacity: 0.5,
+			radius: value * 2000
+		}).addTo(mymap);
+
+		circle.bindPopup("Magnetic field variation at " + site + ": " + value);
+	}
+}
+
+function circleOutlineColour(site){
+	if(site === "MEA"){
+		return("red")
+	} else {
+		return("green")
+	}
+}
+
+function circleFillColour(site){
+	if(site === "MEA"){
+		return("#f03")
+	} else {
+		return("#36BF1D")
+	}								
+}
