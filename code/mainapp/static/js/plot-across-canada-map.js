d60805c2568916d489d7cbe1537e3e43147d9803
Vismay <vismayhr@hotmail.com>
2019-11-03 01:49:04 -0400

    Improved the colours in the heat map and added a legend. The legend has a bug, colour boxes in it are not being displayed.

--- code/mainapp/static/js/plot-across-canada-map.js
+++ code/mainapp/static/js/plot-across-canada-map.js
@@ -38,7 +38,9 @@ function plotAcrossCanadaMap(data){
 			var polygon = L.polygon(polygonBoundary, {color: polygonOutlineColour(val/maxValue), 
 				fillColor: polygonFillColour(val/maxValue), stroke: false, fillOpacity: 0.4}).addTo(mymap);
 
-			popupMessage = "Lat: " + canadaData[p1]['lat'] + "\nlon: " + canadaData[p1]['lon'] + "\nVariation (dB/dt): " + val;
+			popupMessage = "Lat:" + canadaData[p1]['lat'] + " Lon:" + canadaData[p1]['lon'] + " Variation (dB/dt):" + val;
+			popupMessage = "Magnetic field variation (dB/dt) at (" + canadaData[p1]['lat'] + "," + canadaData[p1]['lon'] + ") is: " + val;
+			//popupMessage = "Variation(dB/dt) at (" + canadaData[p1]['lat'] + ""
 
 			polygon.bindPopup(popupMessage);
 
@@ -53,24 +55,54 @@ function plotAcrossCanadaMap(data){
 		c = 0;
 	}
 
+	var legend = L.control({position: 'bottomleft'});
+
+	legend.onAdd = function (mymap) {
+
+	    var div = L.DomUtil.create('div', 'info legend'),
+	    grades = [0, parseFloat(maxValue*0.199).toFixed(2), parseFloat(maxValue*0.399).toFixed(2), parseFloat(maxValue*0.699).toFixed(2), parseFloat(maxValue*0.7).toFixed(2)],
+	    labels = [];
+
+	    console.log("GRADES IS: " + grades);
+
+	    // loop through our density intervals and generate a label with a colored square for each interval
+	    for (var i = 0; i < grades.length; i++) {
+	    	console.log("Color for: " + grades[i] + " is " + polygonFillColour(grades[i] + 0.1));
+	        div.innerHTML +=
+	            '<i style="background:' + polygonFillColour(grades[i] + 0.1) + '"></i> ' +
+	            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+	}
+
+	return div;
+	};
+
+	legend.addTo(mymap);
+
+}
+
+function polygonFillColour(d) {
+    return d > 0.7 	? '#E74C3C' :
+           d > 0.4  ? '#F39C12' :
+           d > 0.2  ? '#2ECC71' :
+           			  '#2E86C1' ;
 }
 
 function polygonOutlineColour(maxValue){
 	if(maxValue < 0.2){
-		return('blue');
+		return('#2E86C1 ');
 	} else if(maxValue < 0.4){
-		return('lime');
+		return('#2ECC71');
 	} else if (maxValue < 0.7){
-		return('yellow');
-	} else return('red');
+		return('#F39C12');
+	} else return('#E74C3C');
 }
 
-function polygonFillColour(maxValue){
+/*function polygonFillColour(maxValue){
 	if(maxValue < 0.2){
-		return('blue');
+		return('#2E86C1 ');
 	} else if(maxValue < 0.4){
-		return('lime');
+		return('#2ECC71');
 	} else if (maxValue < 0.7){
-		return('yellow');
-	} else return('red');
-}
+		return('#F39C12');
+	} else return('#E74C3C');
+}*/
